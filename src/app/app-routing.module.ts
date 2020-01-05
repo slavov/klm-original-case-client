a1f8aebf1ede873f8a76a0bb792a8e141c1ca747
gslavov <georgi.slavov@nl.abnamro.com>
2020-01-05 16:37:22 +0100

    Add statistics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FareDashboardComponent} from './fare-dashboard/fare-dashboard.component';
+import {StatisticDashboardComponent} from './statistic-dashboard/statistic-dashboard.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: FareDashboardComponent},
+  {path: 'statistic', component: StatisticDashboardComponent}
 ];
 
 @NgModule({
--- src/app/statistic-dashboard/statistic-dashboard.component.ts
+++ src/app/statistic-dashboard/statistic-dashboard.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil, tap} from 'rxjs/operators';
+
+import {Statistics} from '../../model/statistics';
+import {TravelServerService} from '../../services/travel-server.service';
 
 @Component({
   selector: 'app-statistic-dashboard',
   templateUrl: './statistic-dashboard.component.html',
   styleUrls: ['./statistic-dashboard.component.css']
 })
-export class StatisticDashboardComponent implements OnInit {
+export class StatisticDashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  statistics: Statistics;
+  isLoading = true;
+
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private travelServerService: TravelServerService) { }
 
   ngOnInit() {
+    this.travelServerService.statistics()
+      .pipe(
+        tap(() => (this.isLoading = false)),
+        takeUntil(this.destroy$))
+      .subscribe(data => {
+        this.statistics = data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
 }
